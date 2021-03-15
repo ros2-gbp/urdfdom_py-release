85b0e3c3c9733503081bfbe464362c42304f709e
Chris Lalancette <clalancette@openrobotics.org>
2021-03-15 14:57:13 +0000

    Imported upstream version '1.1.0' of 'upstream'

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ package_name = 'urdfdom_py'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.1.0',
     package_dir={'': 'src'},
     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/scripts', glob('scripts/display_urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -26,4 +25,9 @@ setup(
     description='Python implementation of the URDF parser.',
     license='BSD',
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'display_urdf = urdf_parser_py.display_urdf:main',
+        ],
+    },
 )
