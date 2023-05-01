be1f843ba627f916bc937b8b8921e57a54eb54be
Chris Lalancette <clalancette@gmail.com>
2023-05-01 21:10:41 +0000

    Rebase from 'upstream'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'urdfdom_py'
 
 setup(
     name=package_name,
-    version='1.2.0',
+    version='1.2.1',
     package_dir={'': 'src'},
     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
     data_files=[
@@ -22,6 +22,8 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
+    maintainer='Chris Lalancette',
+    maintainer_email='clalancette@openrobotics.org',
     description='Python implementation of the URDF parser.',
     license='BSD',
     tests_require=['pytest'],
